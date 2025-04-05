7906d3c5edbcb8238ad7ad15aaf0ba716ee224b6
shruti <shrutisharmaconnect@gmail.com>
2025-04-05 17:58:23 +0530

    added dist in .gitignore

--- dist/models/challenge.model.js
+++
@@ -1,8 +0,0 @@
-// Challenge Model: Stores WebAuthn challenges for user authentication, expiring after 5 minutes.
-import mongoose, { Schema } from 'mongoose';
-const ChallengeSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    payload: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 300 }, // Auto-delete after 300 seconds
-});
-export const Challenge = mongoose.model("Challenge", ChallengeSchema);
